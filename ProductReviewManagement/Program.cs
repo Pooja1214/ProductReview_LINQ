ecd0b64bc2b14f622a5f3429bbd9512de3e317eb
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-20 12:12:33 +0530

    RetrieveTop3Records

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of product review management");
+
+            ReviewManager review = new ReviewManager();
+            review.AddReviews();
+            review.Top3RatedProduct();
+
+            Console.Read();
+        }
+        
+    }
+}
