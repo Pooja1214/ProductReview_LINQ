b51730b6357b33a9a1e4eeae44c03b776d5c7eb0
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-20 11:28:44 +0530

    Adding25ProductReview

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of product review management");
+
+            ReviewManager review = new ReviewManager();
+            review.AddReviews();
+
+            Console.Read();
+        }
+        
+    }
+}
