68255994376b7936ef70fed5d6dccba9f7cf612b
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-20 12:18:11 +0530

    RetrieveRecordRating

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementaion of product review management");
+
+            ReviewManager review = new ReviewManager();
+            review.AddReviews();
+            Console.WriteLine("1.Top3Ratedproduct\n2.RetrivalRecordRatingGreaterThan3");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    review.Top3RatedProduct();
+                    break;
+
+                case 2:
+                    review.IterateMethod(review.RetrivalRecordRatingGreaterThan3());
+                    break;
+
+            }
+           
+
+            Console.Read();
+        }
+        
+    }
+}
